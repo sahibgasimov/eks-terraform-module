1454a85120ba72fc75cc335001c7d78c8e47618a
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-06 21:08:34 -0600

    Create 8-s3-backend.tf

---
+++ 8-s3-backend.tf
@@ -0,0 +1,48 @@
+###############################create these 2 resources first 
+# resource "aws_s3_bucket" "mybucket" {
+#    bucket = "s3statebackend-3212"
+#    versioning {
+#        enabled = true
+#    }
+#    server_side_encryption_configuration {
+#        rule {
+#            apply_server_side_encryption_by_default {
+#                sse_algorithm = "AES256"
+#            }
+#        }
+#    }
+#}
+#
+##create dynamodb
+#resource "aws_dynamodb_rable" "statelock" {
+#    name = "state-lock"
+#    billing_mode = "PAY_PER_REQUEST"
+#    hash_key = "LockID"
+#
+#    attribute {
+#        name = "LockID"
+#        type = "S"
+#    }
+#}
+#
+###############################
+#
+##once above resources are created  add this to your aws provider.tf
+#
+#terraform {
+#    backend "s3" {
+#        bucket = "s3statebackend-3212"
+#        dynamodb_table = "state-lock"
+#        key ="global/mystatefile/terraform.tfstate"
+#        region = "us-east-1"
+#        encrypt = true
+#    }
+#  required_providers {
+#    kubectl = {
+#      source  = "gavinbunney/kubectl"
+#      version = ">= 1.7.0"
+#    }
+#  }
+
+#}
+#https://www.youtube.com/watch?v=o04xfWEouKM&ab_channel=CloudChamp
